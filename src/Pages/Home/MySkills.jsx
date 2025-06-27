4dc66b1cd96a90d010ff371191273e477cf2fbe5
ChandanshiveSupriya <145016796+Eng25-SC@users.noreply.github.com>
2025-06-27 16:56:25 +0530

    update frontend

--- src/Pages/Home/MySkills.jsx
+++ src/Pages/Home/MySkills.jsx
@@ -26,7 +26,7 @@ export default function MySkills() {
   return (
     <section className="skills--section" id="mySkills">
       <div className="portfolio--container">
-        <h2 className="skills--section--heading">My Skills</h2>
+        <h2 className="section--heading">My Skills</h2>
       </div>
       <div className="skills--section--container">
         {skills.map((item, index) => (
